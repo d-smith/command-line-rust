cdc3cc7cf37494145a6d32f99c6d557a6d324476
d-smith <doug.smith.mail@gmail.com>
2023-07-08 20:51:30 -0700

    Added file opening

--- catr/src/lib.rs
+++ catr/src/lib.rs
@@ -1,11 +1,20 @@
 use std::error::Error;
 use clap::{App,Arg};
+use std::fs::File;
+use std::io::{self, BufRead,BufReader};
 
 #[derive(Debug)]
 pub struct Config {
     files: Vec<String>,
     number_lines: bool,
     number_nonblank_lines: bool,
+}
+
+fn open(filename: &str) -> MyResult<Box<dyn BufRead>> {
+    match filename {
+        "-" => Ok(Box::new(BufReader::new(io::stdin()))),
+        _ => Ok(Box::new(BufReader::new(File::open(filename)?))),
+    }
 }
 
 pub fn get_args() -> MyResult<Config> {
@@ -44,6 +53,11 @@ pub fn get_args() -> MyResult<Config> {
 type MyResult<T> = Result<T, Box<dyn Error>>;
 
 pub fn run(config: Config) -> MyResult<()> {
-    dbg!(config);
+    for filename in config.files {
+        match open(&filename) {
+            Err(err) => eprintln!("Failed to open {}: {}", filename, err),
+            Ok(_) => println!("Opened {}", filename),
+        }
+    }
     Ok(())
 }
